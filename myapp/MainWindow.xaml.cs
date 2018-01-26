27b9ccf442e0a6f5064190b4042def8978b9e347
zerkvii <934991794@qq.com>
2018-01-26 08:28:57 +0800

    添加项目文件。

---
+++ myapp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace myapp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Boolean is_max { set; get; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            is_max = false;
+        }
+
+        private void drag(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void close_wd(object sender, MouseButtonEventArgs e)
+        {
+            //this.Close();
+            Application.Current.Shutdown();
+        }
+
+        private void max_wd(object sender, MouseButtonEventArgs e)
+        {
+            if (!is_max)
+            {
+                this.WindowState = WindowState.Maximized;
+                is_max = true;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                is_max = false;
+            }
+        }
+
+        private void min_wd(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+    }
+}
